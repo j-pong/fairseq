fbbde4d7b26a2c89953e0e23d4adacbeb9121850
j-pong <ljh93ljh@gmail.com>
2022-03-02 22:56:20 +0900

    remove online model when they has that

--- fairseq/models/wav2vec/wav2vec2_asr.py
+++ fairseq/models/wav2vec/wav2vec2_asr.py
@@ -322,6 +322,7 @@ class Wav2VecEncoder(FairseqEncoder):
         self.apply_mask = cfg.apply_mask
 
         arg_overrides = {
+            "_name": "wav2vec2",
             "dropout": cfg.dropout,
             "activation_dropout": cfg.activation_dropout,
             "dropout_input": cfg.dropout_input,
@@ -375,7 +376,8 @@ class Wav2VecEncoder(FairseqEncoder):
             self.load_model_weights(state, model, cfg)
 
         model.remove_pretraining_modules()
-        model = model.online_model
+        if hasattr(model, "oneline_model"):
+            model = model.online_model
 
         super().__init__(task.source_dictionary)
 
